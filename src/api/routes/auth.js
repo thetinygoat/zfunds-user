9ad7869ded1a8c399c17137b21f3519997641cf3
Sachin Saini <iamsainisachin@gmail.com>
2019-08-11 16:37:25 +0530

    add otp sending endpoint

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -1,14 +1,34 @@
 const { Router } = require("express");
 const route = Router();
 const AuthService = require("../../services/auth");
-const Joi = require("@hapi/joi");
+const { Otp } = require("../../validators");
+const { ResponseConstructor } = require("../../utils");
+route.post("/otp", async (req, res) => {
+	const otpDTO = req.body;
+	const errors = Otp.validate(otpDTO);
+	if (errors) {
+		return res.json(errors);
+	}
+	const { user } = await AuthService.findIfUserExists({
+		phone: otpDTO.phone
+	});
+	if (user) {
+		const responseObj = ResponseConstructor.constructErrorResponse(
+			409,
+			"conflict",
+			"user already exists"
+		);
+		return res.json(responseObj);
+	}
+	const response = await AuthService.sendOTP(otpDTO.phone);
+	const responseObj = ResponseConstructor.constructSuccessResponse(response);
+	return res.json(responseObj);
+});
 
-route.post("/", async (req, res) => {
+route.post("/signup", async (req, res) => {
 	const userDTO = req.body;
-	// const schema = Joi.object().keys({
-	// 	name: Joi.string()
-	// });
-	const response = await AuthService.signup(userDTO);
+	const errors = Otp.validate(userDTO);
+	const response = await AuthService.sendOTP("9560702890");
 	res.json(response);
 });
 
